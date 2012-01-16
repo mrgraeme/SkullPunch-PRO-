1cecd417dce827addbee5aa16b724bf550efe492
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2012-01-16 21:41:50 +0000

    added guidelines, ingredients, random image and refresh

--- app/controllers/ruby_controller.rb
+++ app/controllers/ruby_controller.rb
@@ -10,8 +10,19 @@ class RubyController < ApplicationController
     @firstword = first[rand(first.length)-1]
     @secondword = second[rand(second.length)-1]
     @thirdword = third[rand(third.length)-1]
-    @fourthword = fourth[rand(first.length)-1]
-
+    @fourthword = fourth[rand(fourth.length)-1]
+    
+    
+    measures = [' handfuls of ', ' pintches of ', ' measures of ', ' pints of ', ' Kilograms of ']
+    ingredients = ['Shark spleen', 'Giant squid heart', 'Warrior monk hair', 'Yeti pelvis', 'Marmot claw', 'Wrestler arm hair', 'Hell fire', 'Italian flair', 'Street fighter spittle' ]
+    
+    @ingredient1 = rand(20).to_s + measures[rand(measures.length)-1] + ingredients[rand(ingredients.length)-1]
+    @ingredient2 = rand(20).to_s + measures[rand(measures.length)-1] + ingredients[rand(ingredients.length)-1]
+    @ingredient3 = rand(20).to_s + measures[rand(measures.length)-1] + ingredients[rand(ingredients.length)-1]
+    @ingredient4 = rand(20).to_s + measures[rand(measures.length)-1] + ingredients[rand(ingredients.length)-1]
+    
+    
+    
   end
   
 end
